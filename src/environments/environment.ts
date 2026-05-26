1a2482991c7e6c3c686153ef5a0ba33e2dab1411
Jose Guillermo Vargas Rodriguez <107493326+josemasster@users.noreply.github.com>
2026-05-26 09:27:31 -0500

    Se ajusta env 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     TERCEROS_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
     CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
     GESTOR_DOCUMENTAL_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/',
-    SABATICOS_CRUD_SERVICE: 'http:/localhost:8080/v1/',
-    SABATICOS_MID_SERVICE: "http://localhost:8081/v1/"
+    SABATICOS_CRUD_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sabaticos_crud/v1/',
+    SABATICOS_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sabaticos_mid/v1/',
 };
