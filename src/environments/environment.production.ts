79d35c2c6b99f745498acebe15b125faf0f081a8
diagutierrezro <diagutierrezro@unal.edu.co>
2026-06-18 14:06:10 -0500

    feat: Varible de entorno para prod

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    deployUrl: 'https://pruebasseguimientosabaticos.portaloas.udistrital.edu.co/',
+    deployUrl: 'https://seguimientosabaticos.portaloas.udistrital.edu.co/',
     TERCEROS_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
     CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
     GESTOR_DOCUMENTAL_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/',
